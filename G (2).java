7e446cd0cf2e929c9bb14641d7a1c2c9d69e71dc
Mitya <mityaprr@mail.ru>
2014-10-26 14:11:34 +0300

    r1

---
+++ G (2).java
@@ -0,0 +1,26 @@
+public class G implements A {
+
+    private double a = 100.500;
+
+    private byte c = 1;
+
+    public int[] ii() {
+        return new int[]{0, 1, 2, 3, 4};
+    }
+
+    public Object rr() {
+        return null;
+    }
+
+    public long dd() {
+        return 99999;
+    }
+
+    public String kk() {
+        return "Yes";
+    }
+
+    public java.util.Random mm() {
+        return new java.util.Random();
+    }
+}
